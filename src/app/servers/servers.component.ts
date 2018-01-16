206b03ace3957ffb17c5803d860734ab6713a214
Frans van der Zijde <vanderzijde@ubib.eur.nl>
2018-01-16 16:54:44 +0100

    property binding, string interpolation and event handling

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-servers',
+  // selector: '.app-servers',
+  // selector: '[app-servers]',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'Server is not created!';
+  
+  constructor() { 
+    setTimeout(() => { this.allowNewServer = true }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onServerCreate() {
+    this.serverCreationStatus = 'Server is created!';
+  }
+}
